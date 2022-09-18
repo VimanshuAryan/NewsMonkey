3717def2472abdb8dfcec212d2b7cfce6ebed164
Vimanshu Aryan <vimanshuaryan@gmail.com>
2022-09-18 19:10:21 +0530

    Merge branch 'master' of https://github.com/VimanshuAryan/news-monkey

---
+++ src/components/NewsItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>News Item</div>
+    )
+  }
+}
+
+export default NewsItem
