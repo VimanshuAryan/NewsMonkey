34998a7591d551be9b244f5bc3572893c614f211
Vimanshu Aryan <vimanshuaryan@gmail.com>
2022-09-19 21:08:20 +0530

    dynamic news cards from newsAPI

--- src/components/News.js
+++ src/components/News.js
@@ -2,17 +2,82 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-  render() {
-    return (
-      <div>
-        This is the news component
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-      </div>
-    )
-  }
+    articles = [
+        {
+            "source": {
+                "id": "google-news-uk",
+                "name": "Google News (UK)"
+            },
+            "author": "Mark Brown",
+            "title": "Police call for calm after ‘serious disorder’ breaks out in Leicester",
+            "description": "Two arrested during latest disturbance in east of city after cricket match last month between India and Pakistan",
+            "url": "https://amp.theguardian.com/uk-news/2022/sep/18/police-community-leaders-crowds-leicester",
+            "urlToImage": "https://i.guim.co.uk/img/media/484881bb217760b519992c8fcf09424e01791f8f/3_106_1023_614/master/1023.jpg?width=1200&height=630&quality=85&auto=format&fit=crop&overlay-align=bottom%2Cleft&overlay-width=100p&overlay-base64=L2ltZy9zdGF0aWMvb3ZlcmxheXMvdGctZGVmYXVsdC5wbmc&enable=upscale&s=d61260b61bf748f298be6f850539113e",
+            "publishedAt": "2022-09-18T11:48:00+00:00",
+            "content": "Police and community leaders have called for calm after large crowds gathered and serious disorder broke out overnight in Leicester.\r\nTwo arrests were made and a large number of people were searched … [+2464 chars]"
+        },
+        {
+            "source": {
+                "id": "espn-cric-info",
+                "name": "ESPN Cric Info"
+            },
+            "author": null,
+            "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
+            "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
+            "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
+            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
+            "publishedAt": "2020-04-27T11:41:47Z",
+            "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
+        },
+        {
+            "source": {
+                "id": "espn-cric-info",
+                "name": "ESPN Cric Info"
+            },
+            "author": null,
+            "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
+            "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
+            "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
+            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
+            "publishedAt": "2020-03-30T15:26:05Z",
+            "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
+        }
+    ]
+    constructor() {
+        super();
+        this.state = {
+            articles: this.articles,
+            loading: false
+        }
+    }
+
+    async componentDidMount() {
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=460ded1b747b454f9d0a1a5c26429216";
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+        this.setState({articles: parsedData.articles});
+    };
+
+    render() {
+        return (
+            <div className="container my-3">
+                <h2>NewsMonkey - Top Headlines</h2>
+                <div className="row my-3">
+                    {this.state.articles.map((element) => {
+                        return <div key = {element.url} className="col-md-4">
+                            <NewsItem
+                                title={element.title ? element.title.slice(0, 45) : ""}
+                                description={element.description ? element.description.slice(0, 88) : ""}
+                                imageUrl={element.urlToImage}
+                                newsUrl={element.url} />
+                        </div>
+                    })}
+
+                </div>
+            </div>
+        )
+    }
 }
 
 export default News
