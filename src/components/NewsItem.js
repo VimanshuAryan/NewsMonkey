0936547055e98e0c1a9d17c090f07faf30931e07
Vimanshu Aryan <vimanshuaryan@gmail.com>
2022-09-21 23:14:28 +0530

    next and prev

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -9,9 +9,9 @@ export class NewsItem extends Component {
                 <div className="card" style={{width: "18rem"}}>
                     <img src={imageUrl ? imageUrl : "https://techcrunch.com/wp-content/uploads/2022/09/07edb091bb329145ee3f3f01476adb40acc6457b.jpg?resize=1200,675"} className="card-img-top" alt="..."/>
                         <div className="card-body">
-                            <h5 className="card-title">{title}...</h5>
-                            <p className="card-text">{description}...</p>
-                            <a href={newsUrl} target = "_blank" rel="noreferrer" className="btn btn-sm btn-primary">Read More</a>
+                            <h5 className="card-title">{title}</h5>
+                            <p className="card-text">{description}</p>
+                            <a href={newsUrl} target = "_blank" rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
                         </div>
                 </div>
             </>
